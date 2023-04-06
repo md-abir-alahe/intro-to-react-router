b10f4989e23a3e9c6a07b05d1ccb9df781652fcc
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-06 17:14:58 +0600

    nested routing in react

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,10 @@ import {
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import ErrorPage from './components/Error/ErrorPage';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
-const router = createBrowserRouter([
+/* const router = createBrowserRouter([
   {
     path: '/',
     element: <App></App>,
@@ -26,7 +28,27 @@ const router = createBrowserRouter([
     path: '/contact',
     element: <Contact></Contact>
   }
+]) */
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
+  }
 ])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
