edf45a33f92034b7c14861db4f474a0f0b860b28
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-07 21:47:58 +0600

    Active route, Landing Spinner and not Found

--- src/main.jsx
+++ src/main.jsx
@@ -73,6 +73,10 @@ const router = createBrowserRouter([
       {
         path: '/contact',
         element: <Contact></Contact>
+      },
+      {
+        path:'*',
+        element: <div>404</div>
       }
     ]
   }
